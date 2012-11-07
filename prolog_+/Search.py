f031920e461370fab4e7e13ad437ff01c0b643ff
Colin Rice <colin@daedrum.net>
2012-11-07 15:11:44 -0500

    Add probabilities to all truth statements

--- prolog_+/Search.py
+++ prolog_+/Search.py
@@ -1,16 +1,18 @@
 from copy import deepcopy
 
 def search(CE, term):
-    if search_true(CE, term):
-        return True
+    prob = search_true(CE, term)
+    if prob is not None:
+        return prob
     term.negated = not term.negated
-    if search_true(CE, term):
-        return False
+    prob = search_true(CE, term)
+    if prob is not None:
+        return 1.0 - prob
     return 'Unknown'
-    
+
 def find_unify(Pred, Statement):
     return Statement.find_unify()
-    
+
 def determination_list(CE, Pred):
     poss = []
     for Statement in CE:
@@ -22,7 +24,7 @@ def determination_list(CE, Pred):
 
 def search_true(CE, Pred, return_mapping=False):
     poss = determination_list(CE, Pred)
-    
+
     for state, mapping in poss:
         if mapping == {}:
             nCE = deepcopy(CE)
@@ -32,8 +34,8 @@ def search_true(CE, Pred, return_mapping=False):
             if state.true(nCE):
                 if return_mapping:
                     return mapping
-                return True
-        
+                return state.left.prob()
+
     for state, mapping in poss:
         if mapping != {}:
             new_state = state.unify(mapping)
@@ -44,35 +46,36 @@ def search_true(CE, Pred, return_mapping=False):
             if new_state.true(nCE):
                 if return_mapping:
                     return mapping
-                return True
-        
-    return False
-    
+                return state.left.prob()
+
+    return None
+
 def test_recurse():
     import Parser
     source = "A(a):A(a)."
     CE = Parser._parse(source)
     assert search(CE, Parser._parse_pred('A(a)')) == 'Unknown'
-    
+
 def test_search():
     import Parser
     source = "A(a):."
     CE = Parser._parse(source)
-    assert search(CE, Parser._parse_pred('A(a)')) == True
-    
+    assert search(CE, Parser._parse_pred('A(a)')) == 1.0
+
     source = "A(X):."
     CE = Parser._parse(source)
-    assert search(CE, Parser._parse_pred('A(a)')) == True
-    
+    assert search(CE, Parser._parse_pred('A(a)')) == 1.0
+
 def test_search_compl():
     import Parser
     source = "A(X):B(b).\nB(b):."
     Pred = Parser._parse_pred('A(a)')
     b = Parser._parse_pred('B(b)')
     CE = Parser._parse(source)
-    
-    assert search(CE, b) == True
-    
+
+    assert search(CE, b) == 1.0
+    assert search(CE, Pred) == 1.0
+
     count_det = 0
     for state in CE:
         if state.determines(Pred) is not None:
@@ -80,19 +83,19 @@ def test_search_compl():
             mapping = state.determines(Pred)
             new_state = state.unify(mapping[0])
     assert count_det == 1
+    print new_state.true(CE)
     assert new_state.true(CE) == True
-    
-    assert search(CE, Pred) == True
-    
+
+
 def test_search_compl_neg():
     import Parser
     source = "A(X):!B(b).\n!B(b):."
     Pred = Parser._parse_pred('A(a)')
     b = Parser._parse_pred('!B(b)')
     CE = Parser._parse(source)
-    
-    assert search(CE, b) == True
-    
+
+    assert search(CE, b) == 1.0
+
     count_det = 0
     for state in CE:
         if state.determines(Pred) is not None:
@@ -101,21 +104,21 @@ def test_search_compl_neg():
             new_state = state.unify(mapping[0])
     assert count_det == 1
     assert new_state.true(CE) == True
-    
-    assert search(CE, Pred) == True
-    
+
+    assert search(CE, Pred) == 1.0
+
 def test_search_unknown_not_true():
     import Parser
     source = "A(X):B(X).\n A(c):."
     Pred = Parser._parse_pred('A(a)')
     CE = Parser._parse(source)
     assert search(CE, Pred) == 'Unknown'
-    
+
     source = "A(a):B(X).\n A(c):."
     Pred = Parser._parse_pred('A(a)')
     CE = Parser._parse(source)
     assert search(CE, Pred) == 'Unknown'
-    
+
 def test_search_and_unify():
     import Parser
     source = "A(a):B(X),C(X).\nB(c):.\nC(d):."
@@ -124,22 +127,22 @@ def test_search_and_unify():
     assert search(CE, Pred) is not True
     assert search(CE, Pred) is not False
     assert search(CE, Pred) == 'Unknown'
-    
+
     source = "A(a):B(X),C(X).\nB(c):.\nC(c):."
     Pred = Parser._parse_pred('A(a)')
     CE = Parser._parse(source)
-    assert search(CE, Pred) is True
-    
-    
+    assert search(CE, Pred) == 1.0
+
+
 def test_search_time():
     import Parser
     source = "A(X):B(b)|C(X).\nB(b):."
     Pred = Parser._parse_pred('A(a)')
     b = Parser._parse_pred('B(b)')
     CE = Parser._parse(source)
-    
-    assert search(CE, b) == True
-    
+
+    assert search(CE, b) == 1.0
+
     count_det = 0
     for state in CE:
         if state.determines(Pred) is not None:
@@ -148,15 +151,15 @@ def test_search_time():
             new_state = state.unify(mapping[0])
     assert count_det == 1
     assert new_state.true(CE) == True
-    
-    assert search(CE, Pred) == True
-    
+
+    assert search(CE, Pred) == 1.0
+
 def test_search_chaining_sub():
     import Parser
     source = "A(a):.\nB(X):A(X).\nC(X),D(X):A(X)."
-    
+
     CE = Parser._parse(source)
-    
+
     count_det = 0
     for state in CE:
         if state.determines(Parser._parse_pred('C(a)')) is not None:
@@ -165,16 +168,11 @@ def test_search_chaining_sub():
             new_state = state.unify(mapping[0])
             #print mapping
             #print new_state
-    
-    assert search(CE, Parser._parse_pred('B(a)')) == True
-    assert search(CE, Parser._parse_pred('!B(a)')) == False
-    assert search(CE, Parser._parse_pred('C(a)')) == True
-    assert search(CE, Parser._parse_pred('!C(a)')) == False
+
+    assert search(CE, Parser._parse_pred('B(a)')) == 1.0
+    assert search(CE, Parser._parse_pred('!B(a)')) == 0.0
+    assert search(CE, Parser._parse_pred('C(a)')) == 1.0
+    assert search(CE, Parser._parse_pred('!C(a)')) == 0.0
     assert search(CE, Parser._parse_pred('C(b)')) == 'Unknown'
     assert search(CE, Parser._parse_pred('!C(b)')) == 'Unknown'
-    
 
-    
-    
-    
-    
--- prolog_+/Types.py
+++ prolog_+/Types.py
@@ -10,9 +10,9 @@ def isvariable(item):
         assert len(item) > 1
         return item[1].upper() == item[1]
     return item[0].upper() == item[0]
-    
+
 class Predicate():
-    def __init__(self, name, args):
+    def __init__(self, name, args, prob=1.):
         name = name.replace('!!','')
         if name[0] == '!':
             name = name[1:]
@@ -21,38 +21,42 @@ class Predicate():
             self.negated = False
         self.name = name
         self.args = args
-        
+        self.probability = prob
+
+    def prob(self):
+        return self.probability
+
     def hasVariables(self):
         for item in self.args:
             if isinstance(item, Variable):
                 return True
-                
+
         return False
-        
+
     def unify(self, mapping):
-        
+
         new_self = deepcopy(self)
         assert new_self == self
         for i in range(len(self.args)):
             new_self.args[i] = self.args[i].unify(mapping)
         return new_self
-        
-    
+
+
     def true(self, CE, mapping_list):
         assert len(mapping_list) >0
-        
+
         nmapping_list = []
         for mapping in mapping_list:
             new_self = deepcopy(self)
             assert new_self == self
             for i in range(len(self.args)):
                 new_self.args[i] = self.args[i].unify(mapping)
-                
+
             #print 'Searching for %s' % self
             if len(Search.determination_list(CE, new_self)) == 0:
                 continue
-            
-            if Search.search_true(CE, self) is True:
+
+            if Search.search_true(CE, self) is not None:
                 nmapping_uf = Search.search_true(CE, self, return_mapping=True)
                 nmapping = {}
                 for a,b in nmapping_uf.items():
@@ -61,134 +65,141 @@ class Predicate():
                         nmapping[b]=a
                     else:
                         nmapping[a]=b
-                        
+
                 nmapping_list.append(dict(mapping.items() + nmapping.items()))
         #print self, mapping_list, nmapping_list
         return len(nmapping_list) > 0, nmapping_list
-        
+
     def determines(self, other):
         if other is None:
             return None
-            
+
         if self.name != other.name:
             return None
-            
+
         if len(self.args) != len(other.args):
             return None
-            
+
         if self.negated != other.negated:
             return None
-            
+
         mapping = {}
         for i in range(len(self.args)):
-            
+
             if self.args[i] != other.args[i] and not isinstance(self.args[i], Variable) and not isinstance(other.args[i], Variable):
                 return None
             if self.args[i] != other.args[i]:
                 mapping[self.args[i]] = other.args[i]
         return [mapping]
-                
-        
+
+
     def __repr__(self):
         return 'Predicate(' + self.name +', ' + ', '.join(map(str, self.args)) + ')'
-        
+
     def __eq__(self, other):
         if other is None:
             return False
         return type(self) == type(other) and self.name == other.name and self.args == other.args
-        
+
     def __ne__(self, other):
         return not self.__eq__(other)
-        
+
     def __hash__(self):
         return hash(self.name) + sum(map(hash, self.args)) + hash('PREDICATE')
-    
+
 class Atom(Predicate):
-    def __init__(self, name):
+    def __init__(self, name, prob = 1.):
         assert not isvariable(name)
         self.name = name
         self.args = []
-        
+        self.probability = prob
+
     def __eq__(self, other):
         if other is None:
             return False
         return type(self) == type(other) and self.name == other.name and self.args == other.args
-        
+
     def unify(self, mapping):
         new = deepcopy(self)
         assert new == self
         return new
-    
+
+    def prob(self):
+        return self.probability
+
     def __ne__(self, other):
         return not self.__eq__(other)
-        
+
     def __repr__(self):
         return 'Atom("' + self.name + '")'
-        
+
 class Variable():
     def __init__(self, name):
         assert isvariable(name)
         self.name = name
-        
+
     def unify(self, mapping):
         if self in mapping:
             return deepcopy(mapping[self])
         return deepcopy(self)
-    
+
     def __eq__(self, other):
         return type(self) == type(other) and self.name == other.name
-        
+
     def __ne__(self, other):
         return not self.__eq__(other)
-        
+
     def __repr__(self):
         return 'Variable("' + self.name + '")'
-    
+
     def __hash__(self):
         return hash(self.name) + hash('VARIABLE')
-        
+
 class Disjunction():
     def __init__(self, left, right):
         self.left = left
         self.right = right
-        
+
     def determines(self, other):
         return None
-        
+
     def unify(self, mapping):
         new_self = deepcopy(self)
         new_self.left = self.left.unify(mapping)
         new_self.right = self.right.unify(mapping)
         return new_self
-    
+
     def true(self, CE, mapping):
-        val, nmapping = self.left.true(CE, mapping) 
+        val, nmapping = self.left.true(CE, mapping)
         if val == True:
             return True, nmapping
-            
+
         val, mapping = self.right.true(CE, mapping)
-        
+
         return val, mapping
-    
+
+    def prob(self):
+        return self.right.prob() + self.left.prob() - self.right.prob()*self.left.prob()
+
     def __eq__(self, other):
         if other is None:
             return False
         return type(self) == type(other) and self.left == other.left and self.right == other.right
-    
+
     def __ne__(self, other):
         return not self.__eq__(other)
-        
+
     def __repr__(self):
         return 'Disjunction(' + str(self.left) + ", " + str(self.right) + ')'
-        
+
     def __hash__(self):
         return hash(self.left) + hash(self.right) + hash('DISJUNCTION::')
-        
+
 class Conjunction():
     def __init__(self, left, right):
         self.left = left
         self.right = right
-        
+
     def determines(self, other):
         left = self.left.determines(other)
         right = self.right.determines(other)
@@ -200,76 +211,79 @@ class Conjunction():
         if right is not None:
             value.extend(right)
         return value
-        
+
     def unify(self, mapping):
         new_self = deepcopy(self)
         new_self.left = self.left.unify(mapping)
         new_self.right = self.right.unify(mapping)
         return new_self
-        
+
     def true(self, CE, mapping):
-        val, mapping = self.left.true(CE, mapping) 
+        val, mapping = self.left.true(CE, mapping)
         if val != True:
             return False, mapping
         val, mapping = self.right.true(CE, mapping)
         return val, mapping
-        
+
+    def prob(self):
+        return self.left.prob() * self.right.prob()
+
     def __eq__(self, other):
         if other is None:
             return False
         return type(self) == type(other) and self.left == other.left and self.right == other.right
-        
-    
+
+
     def __ne__(self, other):
         return not self.__eq__(other)
-        
+
     def __repr__(self):
         return 'Conjunction(' + str(self.left) + ', ' + str(self.right)  + ')'
-        
+
     def __hash__(self):
         return hash(self.left) + hash(self.right) + hash('CONJUNCTION::')
-        
+
 class Statement():
     def __init__(self, left, right):
         self.left = left
         self.right = right
-            
+
     def determines(self, item):
         return self.left.determines(item)
 
     def __repr__(self):
         return '<Statement ' + str(self.left) + ':' + str(self.right) + ' >'
-        
+
     def __hash__(self):
         return hash(self.left) + hash(self.right)
-        
+
     def unify(self, mapping):
         new_self = deepcopy(self)
         new_self.left = self.left.unify(mapping)
         if self.right is not None:
             new_self.right = self.right.unify(mapping)
         return new_self
-        
+
     def true(self, CE):
         if self.right == None:
             return True
-        
+
         val, mapping = self.right.true(CE, [{}])
         return val
-        
+
     def __eq__(self, other):
         return self.left == other.left and self.right == other.right
-    
+
     def __ne_(self, other):
         return not self.__eq__(self, other)
-        
+
 def test_determines():
     import Parser
     source = "A(a):."
     CE = Parser._parse(source)
     for state in CE:
         assert state.determines(Parser._parse_pred('A(a)')) == [{}]
-        
+
 def test_unify():
     import Parser
     source = "A(X):."
@@ -279,21 +293,21 @@ def test_unify():
         assert len(mapping) > 0
         for item in mapping:
             assert state.unify(item) != state
-            
+
 def test_statement_set_membership():
     import Parser
     source = "A(a):A(a)."
     CE = Parser._parse(source)
     state = Parser._parse_statement("A(a):A(a).")
     assert state in CE
-            
+
 def test_unify_big():
     import Parser
     source = "A(X):."
     CE = Parser._parse(source)
     for state in CE:
         assert str(state.unify({Variable("X"):Atom("a")})) != str(state)
-    
+
 def test_hasVariables():
     import Parser
     pred = "A(X,a,b,c)"
@@ -302,4 +316,4 @@ def test_hasVariables():
     predf = Parser._parse_pred(predf)
     assert pred.hasVariables() == True
     assert predf.hasVariables() == False
-    
+
